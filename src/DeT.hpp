7ed2e80057ad637aecfe83e528d0edd2bb45a629
SEUZFY <zmocheng@gmail.com>
2022-10-03 22:01:57 +0200

    Add code for template class: Point_2. Organize the files.

--- src/DeT.hpp
+++ src/DeT.hpp
@@ -1,8 +1,100 @@
-﻿// DeT.h: 标准系统包含文件的包含文件
-// 或项目特定的包含文件。
+﻿/*
+* Bowyer-Watson algorithm
+* build Delaunay triangulation incrementally
+* 
+* inspired by: https://github.com/jbegaint/delaunay-cpp
+*/
 
 #pragma once
 
+// header files
 #include <iostream>
+#include <vector>
 
-// TODO: 在此处引用程序需要的其他标头。
+// everything goes into delaunay namespace
+namespace delaunay {
+
+
+
+	/* define the epsilon for comparing if two values are the same */
+	const double epsilon = 1e-8;
+
+
+
+	/*
+	* class declarations 
+	* --------------------------------------------------------------------------------------------------------------------------
+	*/
+	template <typename T>
+	class Point_2; // store all the 2D points
+
+	template <typename T>
+	class Edge_2; // store all the 2D edges
+
+	/*--------------------------------------------------------------------------------------------------------------------------*/
+
+
+
+	/*
+	* Point_2 class - store the 2D points
+	* use template
+	*/
+	template <typename T>
+	class Point_2 {
+	public:
+
+		/* constructors */
+		Point_2() : 
+			m_x(T(0)), m_y(T(0)) {} /* default constructor */
+		Point_2(T px, T py) : 
+			m_x(px), m_y(py){} /* constructor with @param */
+
+		template <typename U> /* if the constructor gets different @param types */
+		Point_2(T px, T py) : 
+			m_x(static_cast<T>(px)), m_y(static_cast<T>(py)){}
+
+
+		/* get the x coordinate */
+		T& x() { return m_x; } // return reference, so that we can both access and modify the x coordinate
+		const T& x()const { return m_x; } // const version, for const objects
+
+
+		/* get the y coordinate */
+		T& y() { return m_y; } // return reference, so that we can both access and modify the x coordinate
+		const T& y()const { return m_y; } // const version, for const objects
+
+
+		/* compare if the two points are in the same location */
+		bool operator==(const Point_2<T>& rhs)const {
+			if (abs(m_x - rhs.x()) < epsilon &&
+				abs(m_y - rhs.y()) < epsilon) {
+				return true;
+			}
+			else {
+				return false;
+			}
+
+		} // this function can be declared as `const` since both const and non-const objects can access it
+
+
+		/* ostream overloading, for printing the info of the Point_2<T> objects */
+		friend std::ostream& operator<<(std::ostream& os, const Point_2<T>& p)
+		{
+			os << "(" << p.x() << ", " << p.y() << ")";
+			return os;
+		}
+
+	protected:
+		T m_x; // x coordinate
+		T m_y; // y coordinate
+
+		/* x and y should not be modified directly outside the class */
+	};
+
+
+
+
+} // end of namespace delaunay
+
+
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,3 @@
-#include <iostream>
 #include <string>
 #include <sstream>
 #include <fstream>
@@ -8,49 +7,14 @@
 #include "GL/glew.h"
 #include "GLFW/glfw3.h" // mind the orders, include glew.h first and include glfw3.h next -> to use newest OpenGL functions
 
-using namespace std;
 
 int main()
 {
-    /* Initialize the window pointer with nullptr */
-    GLFWwindow* window(nullptr);
-
-    /* Initialize the library */
-    if (!glfwInit())
-        return -1;
-
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
-    {
-        glfwTerminate();
-        return -1;
-    }
-
-    /* Make the window's context current */
-    glfwMakeContextCurrent(window);
-
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
-    {
-        /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        /* User Modification */
-        glBegin(GL_TRIANGLES);
-        glVertex2f(-0.5f, -0.5f);
-        glVertex2f(0.0f, 0.5f);
-        glVertex2f(0.5f, -0.5f);
-        glEnd();
-
-        /* Swap front and back buffers */
-        glfwSwapBuffers(window);
-
-        /* Poll for and process events */
-        glfwPollEvents();
-    }
-
-    glfwTerminate();
+    delaunay::Point_2<double> p(1.0, 1.0);
+    delaunay::Point_2<double> q(1.0, 1.0 + 1e-5);
+    std::cout << p << std::endl;
+    p.y() = 1.0 + 1e-5;
+    std::cout << (p == q) << std::endl;
 
     return EXIT_SUCCESS;
 }
