b57bfd4dbd1e2f8b4338ea8a6a4f854279baaa4b
SEUZFY <zmocheng@gmail.com>
2022-10-03 12:56:02 +0200

    Add submodules(glfw and glew), add code and test to create a triangle.

--- src/DeT.cpp
+++ src/DeT.cpp
@@ -1,12 +1,58 @@
 ﻿// DeT.cpp: 定义应用程序的入口点。
-//
+
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <fstream>
 
 #include "DeT.h"
+
+#include "GL/glew.h"
+#include "GLFW/glfw3.h" // mind the orders, include glew.h first and include glfw3.h next
 
 using namespace std;
 
 int main()
-{
-	cout << "Hello CMake." << endl;
-	return 0;
+{	
+    /* Initialize the window pointer with nullptr */
+    GLFWwindow* window(nullptr);
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* User Modification */
+        glBegin(GL_TRIANGLES);
+        glVertex2f(-0.5f, -0.5f);
+        glVertex2f(0.0f, 0.5f);
+        glVertex2f(0.5f, -0.5f);
+        glEnd();
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+
+    return EXIT_SUCCESS;
 }
